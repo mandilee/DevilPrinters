76669794d32afc223f7b9d83a75b7e836a5aa124
Mandi Lee <github@mandilee.co.uk>
2017-01-17 15:30:07 +0000

    Version 3 - with call and email functionality

--- app/src/main/java/uk/co/mandilee/devilprinters/MainActivity.java
+++ app/src/main/java/uk/co/mandilee/devilprinters/MainActivity.java
@@ -1,7 +1,13 @@
 package uk.co.mandilee.devilprinters;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +16,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private boolean isTelephonyEnabled() {
+        TelephonyManager tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        return tm != null && tm.getSimState() == TelephonyManager.SIM_STATE_READY;
+    }
+
+
+    public void callUsNow(View v) {
+        if (isTelephonyEnabled()) {
+            Intent i = new Intent(Intent.ACTION_DIAL);
+            String p = "tel:" + getString(R.string.company_phone).replace(" ", "").substring(1);
+            i.setData(Uri.parse(p));
+            startActivity(i);
+        } else {
+            Context context = getApplicationContext();
+            CharSequence text = "Cannot dial number";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+    }
+
+
+    public void emailUsNow(View v) {
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("plain/text");
+        i.putExtra(Intent.EXTRA_EMAIL, new String[]{getString(R.string.email)});
+        i.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject));
+        i.putExtra(Intent.EXTRA_TEXT, getString(R.string.email_body));
+        startActivity(Intent.createChooser(i, ""));
+
+    }
+
 }
