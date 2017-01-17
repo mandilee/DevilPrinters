a946995db383a36e1d4864f319de7f37cc99cd24
Mandi Lee <github@mandilee.co.uk>
2017-01-17 15:35:11 +0000

    Version 3.0.1

--- app/src/main/java/uk/co/mandilee/devilprinters/MainActivity.java
+++ app/src/main/java/uk/co/mandilee/devilprinters/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     public void callUsNow(View v) {
         if (isTelephonyEnabled()) {
             Intent i = new Intent(Intent.ACTION_DIAL);
-            String p = "tel:" + getString(R.string.company_phone).replace(" ", "").substring(1);
+            String p = "tel:" + getString(R.string.company_phone).replace(" ", "");
             i.setData(Uri.parse(p));
             startActivity(i);
         } else {
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
     public void emailUsNow(View v) {
         Intent i = new Intent(Intent.ACTION_SEND);
         i.setType("plain/text");
-        i.putExtra(Intent.EXTRA_EMAIL, new String[]{getString(R.string.email)});
+        i.putExtra(Intent.EXTRA_EMAIL, new String[]{getString(R.string.info_email)});
         i.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject));
         i.putExtra(Intent.EXTRA_TEXT, getString(R.string.email_body));
         startActivity(Intent.createChooser(i, ""));
